ae4a7817c66b60c11e5b5521ce87aa95414c39ee
nellymakinen <92375115+nellymakinen@users.noreply.github.com>
2021-10-13 10:08:51 +0200

    Add files via upload

--- integer.py
+++ integer.py
@@ -3,29 +3,30 @@ import ctypes
 lib = ctypes.cdll.LoadLibrary('./libinteger.so')
 
 class Integer(object):
-	def __init__(self, val):
-		lib.Integer_new.argtypes = [ctypes.c_int]
-		lib.Integer_new.restype = ctypes.c_void_p
-		lib.Integer_get.argtypes = [ctypes.c_void_p]
-		lib.Integer_get.restype = ctypes.c_int
-		lib.Integer_set.argtypes = [ctypes.c_void_p,ctypes.c_int]
-		lib.Integer_delete.argtypes = [ctypes.c_void_p]
-		self.obj = lib.Integer_new(val)
+    def __init__(self, val):
+        lib.Integer_new.argtypes = [ctypes.c_int]
+        lib.Integer_new.restype = ctypes.c_void_p
+        lib.Integer_get.argtypes = [ctypes.c_void_p]
+        lib.Integer_get.restype = ctypes.c_int
+        lib.Integer_set.argtypes = [ctypes.c_void_p,ctypes.c_int]
+        lib.Integer_delete.argtypes = [ctypes.c_void_p]
+        self.obj = lib.Integer_new(val)
 
-	def get(self):
-		return lib.Integer_get(self.obj)
+    def get(self):
+        return lib.Integer_get(self.obj)
 
-	def set(self, val):
-		lib.Integer_set(self.obj, val)
+    def set(self, val):
+        lib.Integer_set(self.obj, val)
         
-	def __del__(self):
-		return lib.Integer_delete(self.obj)
+    def __del__(self):
+        return lib.Integer_delete(self.obj)
   
     def publicfib(self):
         return fib_py(n)
-        
+    
     def fib_py(n):
         if n <= 1:
             return n
         else:
             return(fib_py(n-1) + fib_py(n-2))
+        
