1dd66fd4496f919ff074534274e038cc0b8da057
nellymakinen <92375115+nellymakinen@users.noreply.github.com>
2021-10-13 10:03:40 +0200

    Add files via upload

--- MA4_2.py
+++ MA4_2.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 from integer import Integer
 
 def main():
-	f = Integer(5)
-	print(f.get())
-	f.set(7)
-	print(f.get())
-        plt.plot([1,2,3],[1,2,3]) # do your plotting here
-        plt.savefig("fibonacci_timing.png") 
+    f = Integer(5)
+    print(f.get())
+    f.set(7)
+    print(f.get())
+    plt.plot([1,2,3],[1,2,3]) # do your plotting here
+    plt.savefig("fibonacci_timing.png") 
 if __name__ == '__main__':
-	main()
+    main()
