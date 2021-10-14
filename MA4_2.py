666ada111ccf9188262df03e28b2190513a5c7c6
nellymakinen <92375115+nellymakinen@users.noreply.github.com>
2021-10-14 08:58:27 +0200

    Add files via upload

--- MA4_2.py
+++ MA4_2.py
@@ -3,7 +3,7 @@
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
-# from integer import Integer
+from integer import Integer
 
 def fib_py(n):
         if n <= 1:
@@ -12,15 +12,19 @@ def fib_py(n):
             return(fib_py(n-1) + fib_py(n-2))
         
 def main():
-    #f = Integer(5)
-    #print(f.get())
-    #f.set(7)
-    #print(f.get())
+    length = [x for x in range(35)]
+    fib_by_time = []
+    for i in length:
+        ts = time.time()
+        fib_py(i)
+        fib_py_time.append(time.time() - ts)
+    plt.plot(length, fib_py_time)
+    plt.show()
     print(fib_py(7))
-    plt.plot([1,2,3],[1,2,3]) # do your plotting here
+    #plt.plot([1,2,3],[1,2,3]) # do your plotting here
     plt.savefig("fibonacci_timing.png")
-    #f = Integer(n)
-    #f.fib()
+    f = Integer(5)
+    f.fib()
     
 if __name__ == '__main__':
     main()
