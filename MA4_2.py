dbf064c806068d3fdb63765f00797afa64d02968
nellymakinen <92375115+nellymakinen@users.noreply.github.com>
2021-10-19 09:30:47 +0200

    Add files via upload

--- MA4_2.py
+++ MA4_2.py
@@ -16,32 +16,26 @@ length = [x for x in range(10)] #till tid 45
 fib_py_time = []
 fib_cpp_time = []
 
-# def main():
-#     for i in length:
-#         ts = time.time()
-#         fib_py(i)
-#         fib_py_time.append(time.time() - ts)
-#         
-#     for i in length:
-#         ts = time.time()
-#         f = Integer(i)
-#         f.fib()
-#         fib_cpp_time.append(time.time() - ts)
-#         
-#     plt.plot(length,fib_py_time,fib_cpp_time) # do your plotting here
-#     plt.savefig("fibonacci.png")
-#     plt.show()
+def main():
+    for i in length:
+        ts = time.time()
+        fib_py(i)
+        fib_py_time.append(time.time() - ts)
+        
+    for i in length:
+        ts = time.time()
+        f = Integer(i)
+        f.fib()
+        fib_cpp_time.append(time.time() - ts)
+        
+    plt.plot(length,fib_py_time)
+    plt.plot(length,fib_cpp_time) # do your plotting here
+    plt.savefig("fibonacci.png")
+    plt.show()
     #f = Integer(47) #Integer(47) blir negativt pga overflow, max-värde har nåtts!
     #print(f.fib())
     
-#if __name__ == '__main__':
-#    main()
+if __name__ == '__main__':
+   main()
 
-for i in length:
-    ts = time.time()
-    f = Integer(i)
-    print(f)
-    f.fib()
-    print(f.fib())
-    fib_cpp_time.append(time.time() - ts)
-print(fib_cpp_time)
+
